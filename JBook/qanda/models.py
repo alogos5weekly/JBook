eb6c3fd37c17821a8276c19d7621391493ac05c0
chirayu jain <chirayu.jain98@gmail.com>
2019-07-20 10:46:59 +0530

    changes updated

--- JBook/qanda/models.py
+++ JBook/qanda/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+User = get_user_model()
 class Question(models.Model):
     qid = models.AutoField(primary_key=True)
     question_title = models.CharField(max_length=100)
     question_text = models.TextField(max_length=50000)
     date_posted = models.DateTimeField(auto_now_add = True)
-    posted_by = models.TextField(max_length=20)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=40)
     def save(self, *args, **kwargs):
         self.slug = slugify(self.question_title)
